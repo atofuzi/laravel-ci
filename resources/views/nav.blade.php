036f3fb8ba400ed61a7d1c5b6d6460244646b449
atofuzi <58471852+atofuzi@users.noreply.github.com>
2021-08-28 20:31:30 +0900

    add unit test

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -25,13 +25,11 @@
     @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fas fa-user-circle"></i>
       </a>
       <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button"
-                onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
+        <button class="dropdown-item" type="button" onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
           マイページ
         </button>
         <div class="dropdown-divider"></div>
